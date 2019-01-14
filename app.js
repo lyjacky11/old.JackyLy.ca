63cdda8a9baa36efefc42c5dc6071061a40bb460
lyjacky11 <lyjacky@live.com>
2019-01-13 20:49:15 -0500

    Added Firebase Auth UI

---
+++ app.js
@@ -0,0 +1,135 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBYhCeWnxBnIZh4VHWKULQl1wg62fplD0c",
+  authDomain: "login-auth-ed4eb.firebaseapp.com",
+  databaseURL: "https://login-auth-ed4eb.firebaseio.com",
+  projectId: "login-auth-ed4eb",
+  storageBucket: "login-auth-ed4eb.appspot.com",
+  messagingSenderId: "673661821383"
+};
+firebase.initializeApp(config);
+
+// FirebaseUI config.
+var uiConfig = {
+  callbacks: {
+      // Called when the user has been successfully signed in.
+      'signInSuccessWithAuthResult': function(authResult, redirectUrl) {
+        if (authResult.user) {
+          handleSignedInUser(authResult.user);
+        }
+        if (authResult.additionalUserInfo) {
+          document.getElementById('is-new-user').textContent =
+              authResult.additionalUserInfo.isNewUser ?
+              'New User' : 'Existing User';
+        }
+        // Do not redirect.
+        return false;
+      }
+    },
+  signInFlow: 'popup',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  //tosUrl: '#',
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function() {
+    window.location.assign('#');
+  }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+//ui.disableAutoSignIn();
+
+/**
+ * Displays the UI for a signed in user.
+ * @param {!firebase.User} user
+ */
+var handleSignedInUser = function(user) {
+  document.getElementById('user-signed-in').style.display = 'block';
+  document.getElementById('user-signed-out').style.display = 'none';
+  document.getElementById('firebaseui-auth-container').style.display = 'none';
+  document.getElementById('name').textContent = user.displayName;
+  document.getElementById('email').textContent = user.email;
+  document.getElementById('phone').textContent = user.phoneNumber;
+  if (user.photoURL){
+    var photoURL = user.photoURL;
+    // Append size to the photo URL for Google hosted images to avoid requesting
+    // the image with its original resolution (using more bandwidth than needed)
+    // when it is going to be presented in smaller size.
+    if ((photoURL.indexOf('googleusercontent.com') != -1) ||
+        (photoURL.indexOf('ggpht.com') != -1)) {
+      photoURL = photoURL + '?sz=' +
+          document.getElementById('photo').clientHeight;
+    }
+    document.getElementById('photo').src = photoURL;
+    document.getElementById('photo').style.display = 'block';
+  } else {
+    document.getElementById('photo').style.display = 'none';
+  }
+};
+
+
+/**
+ * Displays the UI for a signed out user.
+ */
+var handleSignedOutUser = function() {
+  document.getElementById('user-signed-in').style.display = 'none';
+  document.getElementById('user-signed-out').style.display = 'block';
+  document.getElementById('firebaseui-auth-container').style.display = 'block';
+  ui.start('#firebaseui-auth-container', uiConfig);
+};
+
+// Listen to change in auth state so it displays the correct UI for when
+// the user is signed in or not.
+firebase.auth().onAuthStateChanged(function(user) {
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('loaded').style.display = 'block';
+  user ? handleSignedInUser(user) : handleSignedOutUser();
+});
+
+/**
+ * Deletes the user's account.
+ */
+var deleteAccount = function() {
+  firebase.auth().currentUser.delete().catch(function(error) {
+    if (error.code == 'auth/requires-recent-login') {
+      // The user's credential is too old. He needs to sign in again.
+      firebase.auth().signOut().then(function() {
+        // The timeout allows the message to be displayed after the UI has
+        // changed to the signed out state.
+        setTimeout(function() {
+          alert('Please sign in again to delete your account.');
+        }, 1);
+      });
+    }
+  });
+};
+
+/**
+ * Initializes the app.
+ */
+var initApp = function() {
+  document.getElementById('sign-out').addEventListener('click', function() {
+    if(confirm('Are you sure you want to sign out?'))
+		firebase.auth().signOut();
+  });
+  document.getElementById('delete-account').addEventListener('click', function() {
+    if(confirm('Are you sure you want to delete this account?'))    
+		deleteAccount();
+  });
+};
+
+window.addEventListener('load', initApp);
