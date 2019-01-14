e9118312251e3bdce012c5421b4bfb30209696fa
lyjacky11 <lyjacky@live.com>
2019-01-13 21:08:37 -0500

    Added phone auth support

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var uiConfig = {
     //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     //firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
     //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
   ],
   // tosUrl and privacyPolicyUrl accept either url string or a callback
