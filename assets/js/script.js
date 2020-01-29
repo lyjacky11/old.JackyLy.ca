d7a20e356f4461998bfd523004aa6cc2b005686b
lyjacky11 <lyjacky@live.com>
2020-01-28 19:11:08 -0500

    Added Typed.JS library

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,13 @@ function openNav() {
 function closeNav() {
   document.getElementById("sideMenu").style.height = "0";
 }
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 75,
+    startDelay: 500,
+    backSpeed: 50,
+    backDelay: 1250,
+    loop: true,
+    loopCount: Infinity,
+  });
